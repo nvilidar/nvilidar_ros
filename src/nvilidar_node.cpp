c5e71cd3198d212c956d6f5acd64d240bfd85a3d
xiaoxiangyu <xiaoxiangyu3@163.com>
2021-12-09 09:47:36 +0800

    1. fix the high cpu usage 2.optimize timestamp 3.fix some time quit timeout

--- src/nvilidar_node.cpp
+++ src/nvilidar_node.cpp
@@ -1,29 +1,17 @@
 #include "ros/ros.h"
-#include "sensor_msgs/LaserScan.h"
-#include "CNviLidar.h"
 #include <vector>
 #include <iostream>
 #include <string.h>
+#include "sensor_msgs/LaserScan.h"
+#include "nvilidar_driver_serialport.h"
+#include "nvilidar_dataprocess.h"
 #include "mysignal.h"
-
 using namespace nvilidar;
 
-#define ROSVerision "1.0.1"
+#define ROSVerision "1.0.2"
 
-
-std::vector<float> split(const std::string &s, char delim) 
+int main(int argc, char * argv[]) 
 {
-    std::vector<float> elems;
-    std::stringstream ss(s);
-    std::string number;
-    while(std::getline(ss, number, delim)) {
-        elems.push_back(atof(number.c_str()));
-    }
-    return elems;
-}
-
-
-int main(int argc, char * argv[]) {
     ros::init(argc, argv, "nvilidar_node"); 
     printf(" _   ___      _______ _      _____ _____          _____ \n");
     printf("| \\ | \\ \\    / /_   _| |    |_   _|  __ \\   /\\   |  __ \\\n");
@@ -34,99 +22,79 @@ int main(int argc, char * argv[]) {
     printf("\n");
     fflush(stdout);
 
-    //sig init 
-    nvilidar::SigInit();
-  
-    std::string port;
-    int baudrate = 921600;
-    std::string frame_id;
-    bool reversion, resolution_fixed;
-    bool auto_reconnect;
-    double angle_max,angle_min;
-    result_t op_result;
-    std::string list;
-    std::vector<float> ignore_array;  
-    double max_range, min_range;
-    double frequency;
-    int samp_rate = 20;
-    bool inverted = false;
-    bool isSingleChannel = false;
+    //参数配置 
+    Nvilidar_UserConfigTypeDef  cfg;
+    Nvilidar_UserConfigTypeDef  cfg_default;
+    //获取默认参数  如需要修改 可以进行修改  
+	LidarDefaultUserConfig(cfg_default);
 
     ros::NodeHandle nh;
     ros::Publisher scan_pub = nh.advertise<sensor_msgs::LaserScan>("scan", 1000);
     ros::NodeHandle nh_private("~");
-    nh_private.param<std::string>("port", port, "/dev/nvilidar"); 
-    nh_private.param<int>("baudrate", baudrate, 921600); 
-    nh_private.param<std::string>("frame_id", frame_id, "laser_frame");
-    nh_private.param<bool>("resolution_fixed", resolution_fixed, "true");
-    nh_private.param<bool>("auto_reconnect", auto_reconnect, "true");
-    nh_private.param<bool>("reversion", reversion, "false");
-    nh_private.param<double>("angle_max", angle_max , 180);
-    nh_private.param<double>("angle_min", angle_min , -180);
-    nh_private.param<double>("range_max", max_range , 64.0);
-    nh_private.param<double>("range_min", min_range , 0.01);
-    nh_private.param<double>("frequency", frequency , 10.0);
-    nh_private.param<std::string>("ignore_array",list,"");
-    nh_private.param<int>("samp_rate", (samp_rate), (samp_rate));
-    nh_private.param<bool>("isSingleChannel", isSingleChannel, isSingleChannel);
- 
 
-    ignore_array = split(list ,',');
-    if(ignore_array.size()%2){
-        ROS_ERROR_STREAM("ignore array is odd need be even");
-    }
+    //读取雷达配置 从rviz文件内 如果里面有配置对应参数  则走里面的数据。如果没有，则直接取“nvilidar_dataprocess.h”里面的默认参数配置信息
+    nh_private.param<std::string>("serialport_name", cfg.serialport_name, cfg_default.serialport_name); 
+    nh_private.param<int>("serialport_baud", cfg.serialport_baud, cfg_default.serialport_baud); 
+    nh_private.param<std::string>("frame_id", cfg.frame_id, cfg_default.frame_id);
+    nh_private.param<bool>("resolution_fixed", cfg.resolution_fixed, cfg_default.resolution_fixed);
+    nh_private.param<bool>("auto_reconnect", cfg.auto_reconnect, cfg_default.auto_reconnect);
+    nh_private.param<bool>("reversion", cfg.reversion, cfg_default.reversion);
+    nh_private.param<bool>("inverted", cfg.inverted, cfg_default.inverted);
+    nh_private.param<double>("angle_max", cfg.angle_max , cfg_default.angle_max);
+    nh_private.param<double>("angle_min", cfg.angle_min , cfg_default.angle_min);
+    nh_private.param<double>("range_max", cfg.range_max , cfg_default.range_max);
+    nh_private.param<double>("range_min", cfg.range_min , cfg_default.range_min);
+    nh_private.param<double>("aim_speed", cfg.aim_speed , cfg_default.aim_speed);
+    nh_private.param<int>("sampling_rate", cfg.sampling_rate, cfg_default.sampling_rate);
+    nh_private.param<bool>("sensitive",      cfg.sensitive, cfg_default.sensitive);
+    nh_private.param<int>("tailing_level",  cfg.tailing_level, cfg_default.tailing_level);
+    nh_private.param<double>("angle_offset",  cfg.angle_offset, cfg_default.angle_offset);
+    nh_private.param<bool>("single_channel",  cfg.single_channel, cfg_default.single_channel);
+    nh_private.param<std::string>("ignore_array_string",  cfg.ignore_array_string, cfg_default.ignore_array_string);
 
-    for(uint16_t i =0 ; i < ignore_array.size();i++){
-        if(ignore_array[i] < -180 && ignore_array[i] > 180){
-            ROS_ERROR_STREAM("ignore array should be between 0 and 360");
-        }
-    }
+    //同步数据至雷达参数列表  
+    LidarParaSync(cfg);
 
-    CNviLidar laser;
+    
+	CircleDataInfoTypeDef node_circle;
+	Nvilidar_DeviceInfo info;
+	static uint32_t  no_response_times = 0;
 
-    if(angle_max < angle_min){
-        double temp = angle_max;
-        angle_max = angle_min;
-        angle_min = temp;
-    }
+    nvilidar::LidarDriverSerialport laser(cfg);
 
     ROS_INFO("[NVILIDAR INFO] Now NVILIDAR ROS SDK VERSION:%s .......", ROSVerision);
-    laser.setSerialPort(port);
-    laser.setSerialBaudrate(baudrate);
-    laser.setMaxRange(max_range);
-    laser.setMinRange(min_range);
-    laser.setMaxAngle(angle_max);
-    laser.setMinAngle(angle_min);
-    laser.setReversion(reversion);
-    laser.setFixedResolution(resolution_fixed);
-    laser.setAutoReconnect(auto_reconnect);
-    laser.setScanFrequency(frequency);
-    laser.setIgnoreArray(ignore_array);
-    laser.setInverted(inverted);
-    laser.setSingleChannel(isSingleChannel);
-    bool ret = laser.LidarInitialize();
-    if (ret) {
-        ret = laser.LidarTurnOn();
-        if (!ret) {
-            ROS_ERROR("Failed to start scan mode!!!");
+
+    //初始化 变量定义 
+    bool ret = laser.LidarInit();
+    if (ret) 
+    {
+        //启动雷达 
+        ret = laser.StartScan();
+        if (!ret) 
+        {
+            ROS_ERROR("Failed to start Scan!!!");
         }
-    } else {
+    } 
+    else 
+    {
         ROS_ERROR("Error initializing NVILIDAR Comms and Status!!!");
     }
     ros::Rate rate(50);
 
-    while (ret && ros::ok && nvilidar::isOK()) 
+    while (ret && ros::ok()) 
     {
-       // bool hardError;
          LidarScan scan;
-         if(laser.LidarSamplingProcess(scan ))
+         if(laser.waitCircleResponse(node_circle))
          {
+             //点集格式转换 
+			LidarSamplingData(cfg, node_circle, scan);
+
             sensor_msgs::LaserScan scan_msg;
             ros::Time start_scan_time;
             start_scan_time.sec = scan.stamp/1000000000ul;
             start_scan_time.nsec = scan.stamp%1000000000ul;
             scan_msg.header.stamp = start_scan_time;
-            scan_msg.header.frame_id = frame_id;
+            scan_msg.header.frame_id = "laser_frame";
             scan_msg.angle_min =(scan.config.min_angle);
             scan_msg.angle_max = (scan.config.max_angle);
             scan_msg.angle_increment = (scan.config.angle_increment);
@@ -135,6 +103,7 @@ int main(int argc, char * argv[]) {
             scan_msg.range_min = (scan.config.min_range);
             scan_msg.range_max = (scan.config.max_range);
             int size = (scan.config.max_angle - scan.config.min_angle)/ scan.config.angle_increment + 1;
+            
             scan_msg.ranges.resize(size);
             scan_msg.intensities.resize(size);
             for(int i=0; i < scan.points.size(); i++) {
@@ -150,8 +119,7 @@ int main(int argc, char * argv[]) {
         rate.sleep();
         ros::spinOnce();
     }
-
-    laser.LidarTurnOff();
+    laser.LidarStopScan();
     ROS_INFO("[NVILIDAR INFO] Now NVILIDAR is stopping .......");
     laser.LidarCloseHandle();
     return 0;
